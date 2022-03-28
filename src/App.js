d9629e18c12709f50b03049ee948b8dab9e7104e
afbelardi <afbelardi@gmail.com>
2022-03-28 19:46:21 -0400

    styled header and search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
-import About from './pages/About';
 import Header from './components/Header';
+import axios from 'axios';
+
 
 
 function App() {
+
+  const [state, setState] = useState({})
+  const [parks, setParks] = useState([])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const stateInput = event.target.value
+  }
 
   return (
     <div className="main-container">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../App.css';
 
-function Header() {
+function Header(props) {
   return (
     <div className="header-div">
+        <h1 className="national-park-title">National Park Finder</h1>
+        <form className="form" onSubmit={props.handleSubmit}>
+            <input 
+            className="header-input" 
+            placeholder="CA, WY, FL"
+            />
+                
+        </form>
     </div>
   )
 }
