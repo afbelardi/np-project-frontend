557764bfbfddc680ae652b303f7b0f05842bbd66
afbelardi <afbelardi@gmail.com>
2022-05-01 15:32:06 -0400

    added email input

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,28 +6,34 @@ export default function Register() {
     <div class="w-full max-w-xs">
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
   <div class="mb-4">
-      <label class="text-gray-700 text-sm font-bold mb-2" for="name">
+      <label class="text-gray-700 text-sm font-bold mb-2">
         Name
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Name" />
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Name" />
+    </div>
+    <div class="mb-4">
+      <label class="text-gray-700 text-sm font-bold mb-2">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Email" />
     </div>
     <div class="mb-4">
       <label class="text-gray-700 text-sm font-bold mb-2">
         Username
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Username" />
     </div>
     <div class="mb-4">
       <label class="text-gray-700 text-sm font-bold mb-2">
         Password
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="******************" />
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="******************" />
     </div>
     <div class="mb-6">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+      <label class="block text-gray-700 text-sm font-bold mb-2">
         Confirm Password
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" / >
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="******************" / >
       {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
     </div>
     <div class="flex items-center justify-between">
