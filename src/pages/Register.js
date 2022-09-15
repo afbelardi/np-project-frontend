3f2e6a181bb09b9a338233321e5773c64f5f97cb
afbelardi <afbelardi@gmail.com>
2022-09-15 15:53:32 -0400

    added toast notifications for registering

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Register() {
 
@@ -9,11 +11,14 @@ export default function Register() {
     const password2 = useRef(null);
 
     const [wrongPassword, checkWrongPassword] = useState(false);
+    const [usernameTaken, checkUsernameTaken] = useState(false);
+    const [responseMessage, setResponseMessage] = useState('');
 
     const handleRegister = async (e) => {
         try {
             if (password.current.value !== password2.current.value) {
               checkWrongPassword(true);
+              toast("Passwords do not match")
             } else {
                 const response = await fetch(`http://localhost:8000/api/users/register`, {
             method: 'POST',
@@ -29,56 +34,58 @@ export default function Register() {
         })
         const data = await response.json();
         checkWrongPassword(false);
-        console.log(data);
-            }
-        
+        setResponseMessage(data.message)
+           }
+         
         } catch(error) {
             console.error(error);
         }
     };
 
+    const notify = () => {
+      toast(`${responseMessage}`)
+    }
+  
+
   return (
-    <div className="bg-gray-800 h-screen flex justify-center items-center">
+    <div className="flex items-center justify-center h-screen bg-gray-800">
     <div className="w-full max-w-xs">
-  <form className="bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-4">
+  <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-2xl">
   <div className="mb-4">
-      <label className="text-gray-700 text-sm font-bold mb-2">
+      <label className="mb-2 text-sm font-bold text-gray-700">
         Name
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ref={name}  placeholder="Name" />
+      <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" ref={name}  placeholder="Name" />
     </div>
     <div className="mb-4">
-      <label className="text-gray-700 text-sm font-bold mb-2">
+      <label className="mb-2 text-sm font-bold text-gray-700">
         Email
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ref={email} placeholder="Email" />
+      <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" ref={email} placeholder="Email" />
     </div>
     <div className="mb-4">
-      <label className="text-gray-700 text-sm font-bold mb-2">
+      <label className="mb-2 text-sm font-bold text-gray-700">
         Username
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ref={username} placeholder="Username" />
+      <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" ref={username} placeholder="Username" />
     </div>
     <div className="mb-4">
-      <label className="text-gray-700 text-sm font-bold mb-2">
+      <label className="mb-2 text-sm font-bold text-gray-700">
         Password
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" ref={password} type="password" placeholder="******************" />
+      <input className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" ref={password} type="password" placeholder="******************" />
     </div>
     <div className="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2">
+      <label className="block mb-2 text-sm font-bold text-gray-700">
         Confirm Password
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" ref={password2} type="password" placeholder="******************" / >
+      <input className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" ref={password2} type="password" placeholder="******************" / >
       {/* <p class="text-red-500 text-xs italic">Please choose a password.</p> */}
     </div>
-    {wrongPassword ? (
-      <div>The password does not match!</div>
-    ) :
-    ''
-  }
+  
+  
     <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleRegister} type="button">
+      <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline" onClick={() => {handleRegister(); notify();}} type="button">
         Register
       </button>
       {/* <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
@@ -87,6 +94,17 @@ export default function Register() {
     </div>
   </form>
 </div>
+{
+    responseMessage ? (
+      <ToastContainer
+      position='top-right'
+      autoClose={5000}
+      closeOnClick
+     / >
+
+    ) :
+    ''
+   }
 </div>
   )
 }
